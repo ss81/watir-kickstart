f442c7d08912615addfaf48f280903f6102452f9
Sergei Stryukov <sergei.stryukov@gmail.com>
2012-12-25 20:18:51 +0200

    Update helpers/pcms_accounts.rb

--- helpers/pcms_accounts.rb
+++ helpers/pcms_accounts.rb
@@ -26,7 +26,14 @@ module PCMS
           :shipping_state => "VIC",
           :shipping_zip => "2601",
           :shipping_country => "Australia",
-          :shipping_phone => "5555-555-555"
+          :shipping_phone => "5555-555-555",
+
+          :card_name => "ERIK Marzett",
+          :card_type => "V",
+          :card_num => "4111111111111111",
+          :card_expire_date => "12",
+          :card_year => "2014",
+          :card_verification_num => "111"
       }
     end
   end
