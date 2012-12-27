8dfcb9bb16c6820c6243f6a684e264435f9c5fc6
Sergei Stryukov <sergei.stryukov@gmail.com>
2012-12-27 20:16:46 +0200

    Update helpers/config.rb

--- helpers/config.rb
+++ helpers/config.rb
@@ -15,44 +15,54 @@ RSpec.configure do |config|
   ###############################
   # Actions before and after ALL
   ###############################
-  config.before(:all) {
+  config.before :all do
     @site_url = "http://example.com/" # TODO Change me
 
     @headless = Headless.new
     @headless.start
-  }
+  end
 
-  config.after(:all) {
+  config.after :all do
     @headless.destroy
-  }
+  end
 
 
   #################################
   # Actions before and after EACH
   #################################
-  config.before(:each) {
-    # Open a site in browser and resize the browser's window.
+  config.before :each do
     @browser = Watir::Browser.new :ff
     @browser.driver.manage.timeouts.implicit_wait = 60
-    @browser.goto @site_url
     @browser.window.move_to(0, 0)
     @browser.window.resize_to(1400, 1000)
-    
-    # Storage for test screenshots.
-    example.metadata[:screenshot] = []
-  }
 
-  config.after(:each) {
+    @browser.goto "http://google.com"
+    @headless.video.start_capture
+
+    @browser.goto @site_url
+
+    @screenshots = []
+    @path_to_report = File.expand_path("..", File.dirname(__FILE__)) + "/reports/" + example.metadata[:example_group][:description_args][0].to_s.downcase + "/"
+    @test_name = example.metadata[:description].gsub(/[.,=]/, " ").strip.gsub(/\s+/, "-").downcase
+
+    if not File.directory? @path_to_report
+      FileUtils.mkdir @path_to_report
+    end
+  end
+
+  config.after :each do
     # Remove screenshots after successful test.
     if example.exception == nil
-      example.metadata[:screenshot].each do |screenshot|
+      @screenshots.each do |screenshot|
         if File.exist?(screenshot)
           File.delete(screenshot)
         end
       end
+    else 
+      @headless.video.stop_and_save @path_to_report + @test_name + "--" + Time.now.strftime("%Y%m%d_%H%M%S") + ".mov"
     end
 
     # Close browser.
     @browser.close
-  }
+  end
 end
